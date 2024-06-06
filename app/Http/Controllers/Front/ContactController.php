905a9a08205801bd348246091db7745856b1d765
Tural Babirov <tural.babirov@mail.com>
2024-06-06 19:14:10 +0400

    Front parcalandi - 06.06.2024

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -9,15 +9,6 @@ class ContactController extends Controller
 {
     public function store(Request $request)
     {
-        // İletişim formu verilerini işleyin
-        $message = new Message();
-        $message->name = $request->input('name');
-        $message->email = $request->input('email');
-        $message->subject = $request->input('subject');
-        $message->message = $request->input('message');
-        $message->save();
-
-        // İletişim formu gönderildikten sonra yönlendirme yapın
-        return redirect()->back()->with('success', 'Mesajınız başarıyla gönderildi!');
+        return view('front.pages.index');
     }
 }
